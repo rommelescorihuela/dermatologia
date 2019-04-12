e89efec34e25d5f994e76201b7deb64083395a6f
rommel escorihuela <rommelescorihuela@gmail.com>
2019-04-12 16:01:20 -0400

    correcciones de la vista de paciente

--- views/paciente/_form.php
+++ views/paciente/_form.php
@@ -8,44 +8,75 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="paciente-form">
-
+<div class="row">
+<div class="col-md-1">
+</div>
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'ci')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'apellido')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'fechanac')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'edad')->textInput() ?>
-
-    <?= $form->field($model, 'edocivil')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'direccion')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'telefono1')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'telefono2')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'telefono3')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'correo')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'profesion')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'sexo')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'referencia')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'motivo')->textInput(['maxlength' => true]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+<div class="row">
+    <div class="col-md-2">
+        <?= $form->field($model, 'ci')->textInput(['maxlength' => true]) ?>
+    </div>        
+    <div class="col-md-2">
+        <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
     </div>
+    <div class="col-md-2">
+        <?= $form->field($model, 'apellido')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-md-2">
+        <?= $form->field($model, 'fechanac')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-md-2">
+        <?= $form->field($model, 'edad')->textInput() ?>
+    </div>
+</div>
+<div class="col-md-1">
+</div>
+<div class="row">
+    <div class="col-md-2">
+        <?= $form->field($model, 'edocivil')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-md-2">
+        <?= $form->field($model, 'direccion')->textArea(['maxlength' => true]) ?>
+    </div>    
+    <div class="col-md-2">
+        <?= $form->field($model, 'telefono1')->textInput(['maxlength' => true]) ?>
+    </div>    
+    <div class="col-md-2">
+        <?= $form->field($model, 'telefono2')->textInput(['maxlength' => true]) ?>
+    </div>    
+    <div class="col-md-2">
+        <?= $form->field($model, 'telefono3')->textInput(['maxlength' => true]) ?>
+    </div>
+</div>
+<div class="col-md-1">
+</div>
+<div class="row"> 
+    <div class="col-md-2">
+        <?= $form->field($model, 'correo')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-md-2">
+        <?= $form->field($model, 'profesion')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-md-2">
+        <?= $form->field($model, 'sexo')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-md-2">
+        <?= $form->field($model, 'referencia')->textInput(['maxlength' => true]) ?>
+    </div>
+    <div class="col-md-2">
+        <?= $form->field($model, 'motivo')->textInput(['maxlength' => true]) ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12" align="center">
+        
+    <div class="form-group">
+        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
+    </div>
+    </div>
+</div>
 
     <?php ActiveForm::end(); ?>
+
 
 </div>
